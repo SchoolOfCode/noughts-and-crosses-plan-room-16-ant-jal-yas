8d762106f0222e0fa8018dba41abb4265691240c
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-09 16:27:24 +0000

    game working except for diplaying winner message

--- tic-tac-toe/src/Components/Board/index.js
+++ tic-tac-toe/src/Components/Board/index.js
@@ -1,15 +1,26 @@
 import Square from "../Square";
 import css from "./index.module.css";
 
-function Board({ board, onClick, turn }) {
+function Board({ board, onClick, turn, upd }) {
   //how to use function?
 
   return (
-    <div className={css.gridContainer}>
-      {board.map((item, ind) => (
-        <Square text={item} key={ind} value={ind} onClick={onClick} turn={turn}/>
-      ))}
-    </div>
+    <>
+      <div className={css.gridContainer}>
+        {board.map((item, ind) => (
+          <Square
+            text={item}
+            key={ind}
+            value={ind}
+            onClick={onClick}
+            turn={turn}
+          />
+        ))}
+      </div>
+      <>
+        <button onClick={() => upd(Array(9).fill(null))}>Reset</button>
+      </>
+    </>
   );
 }
 
--- tic-tac-toe/src/Components/Game/index.js
+++ tic-tac-toe/src/Components/Game/index.js
@@ -1,34 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Board from "../Board";
+import css from "./index.module.css";
 
 export default function Game() {
   const [board, updateBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState("x");
 
-  useEffect(() => {}, [Board]);
+  function getWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      let [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = getWinner(board);
+
+  const [turn, setTurn] = useState("X");
 
   function makeAMove(playerMove, key) {
+    if (winner) {
+      alert(`${playerMove} wins`);
+      return;
+    }
     const updatedArray = [
       ...board.slice(0, key),
       playerMove,
       ...board.slice(key + 1),
     ];
-    if(board[key] !== null){
-      alert("Im not empty!")
-      return
+    if (board[key] !== null) {
+      alert("Im not empty!");
+      return;
     }
     updateBoard(updatedArray);
 
-    if (turn === "x") {
-      setTurn("o");
+    if (turn === "X") {
+      setTurn("O");
     } else {
-      setTurn("x");
+      setTurn("X");
     }
   }
 
   return (
-    <div>
-      <Board board={board} onClick={makeAMove} turn={turn} />
+    <div className={css.container}>
+      <Board board={board} onClick={makeAMove} upd={updateBoard} turn={turn} />
     </div>
   );
 }
